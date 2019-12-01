4df294c35c2260ec7c211671799dd00caeb76f3b
MIAIONE <46324080+MIAIONE@users.noreply.github.com>
2019-12-01 10:35:33 +0800

    Add files via upload

---
+++ files/download/ProcessManager/Main.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Console
+Module Main
+    ''' <summary>
+    ''' 外部调用示例
+    ''' </summary>
+    Sub Main()
+        On Error Resume Next
+        If Command() <> Nothing Then
+            Dim Newr = Replace(Command().ToUpper, ".EXE", "").Trim
+            If ProcessCheck(Newr) > 0 Then
+                KillProcess(Newr)
+            End If
+        Else
+            Do
+                WriteLine("Please Enter Process Name : ")
+                Dim Readthing As String
+                Readthing = ReadLine()
+                If ProcessCheck(Readthing) > 0 Then
+                    KillProcess(Readthing)
+                End If
+            Loop
+        End If
+    End Sub
+    Public Function ProcessCheck(ByVal ProcessName As String) As Integer
+        On Error Resume Next
+        Dim Apps As String = ProcessName.ToUpper.Replace(".EXE", "").Trim
+        Return Process.GetProcessesByName(Apps).Length
+    End Function
+
+    Public Function KillProcess(ByVal ProcessNameObject As String)
+        On Error Resume Next
+        Dim ProcessListes = Process.GetProcessesByName(ProcessNameObject)
+        For Each TEMProcess As Process In ProcessListes
+            TEMProcess.Kill()
+        Next
+        Return Nothing
+    End Function
+End Module
